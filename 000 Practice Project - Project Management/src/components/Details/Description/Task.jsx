446a8e7e5a354c56f542f2455e82925c7a7a048b
myupopov <93000608+myupopov@users.noreply.github.com>
2025-08-31 16:32:08 +0300

    Delete 000 Practice Project - Project Management directory

--- 000 Practice Project - Project Management/src/components/Details/Description/Task.jsx
+++
@@ -1,8 +0,0 @@
-export default function Task( {taskName, onTaskDelete}) {
-  return (
-    <li className="flex justify-between my-4">
-      <span>{taskName}</span>
-      <button onClick={onTaskDelete} className="text-stone-700 hover:text-red-500">Clear</button>
-    </li>
-  );
-}
--- 000 Practice Project - Project Management/src/components/Projects/Project.jsx
+++
@@ -1,7 +0,0 @@
-export default function Project({ project, onProjectSelect }) {
-  return (
-    <div className="mt-6">
-      <button onClick={() => onProjectSelect(project)} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">{project.title}</button>
-    </div>
-  );
-}
